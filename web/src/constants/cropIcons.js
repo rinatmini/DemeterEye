89f540aa4fae9113951b7d3798703775a9fa26b9
Artem Halushka <i-artem.halushka@armada.ai>
2025-10-05 00:40:43 -0700

    add icons for crop on FieldDetails

---
+++ web/src/constants/cropIcons.js
@@ -0,0 +1,7 @@
+export const cropIcons = {
+  potato: "/crop_icons/potato.svg",
+  soybean: "/crop_icons/soy_bean.svg",
+  "sugar beet": "/crop_icons/sugar_beet.svg",
+  tomato: "/crop_icons/tomato.svg",
+  wheat: "/crop_icons/wheat.svg",
+};
